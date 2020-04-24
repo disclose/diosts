846e3c2e4a3695b2dc400060c150711c717f1a7b
Luke Stephens (hakluke) <13975395+hakluke@users.noreply.github.com>
2020-04-24 13:02:50 +1000

    better concurrency model, -t option

--- main.go
+++ main.go
@@ -1,89 +1,87 @@
-package main 
+// Receives lines from stdin, processes them with a user-defined number of threads
+
+package main
+
 import (
-        "bufio"
-        "strings"
-        "crypto/tls"
-        "fmt"
-        "log"
-        "net"
-        "net/http"
-        "io/ioutil"
-        "os"
-        "sync"
-        "syscall"
-        "time"
+    "bufio"
+    "flag"
+    "fmt"
+    "sync"
+    "os"
+    "net/http"
+    "crypto/tls"
+    "net"
+    "time"
+    "io/ioutil"
+    "log"
+    "strings"
 )
 
-func ulimit() (uint64, error) {
-        var rLimit syscall.Rlimit
-        err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit)
-        if err != nil {
-                return 0, err
-        }
-        return rLimit.Cur, nil
-}
 
 func main() {
-        maxFileDescriptors, err := ulimit()
-        if err != nil {
-                log.Fatal(err)
-        }
-        if maxFileDescriptors-100 < 0 {
-                log.Fatalf("maxFileDescriptors==%d is not enough", maxFileDescriptors)
-        }
+        concurrencyPtr := flag.Int("t", 8, "Number of threads to utilise. Default is 8.")
+        flag.Parse()
 
-        var wg sync.WaitGroup
-        lock := make(chan struct{}, maxFileDescriptors-100)
         client := &http.Client{Transport: &http.Transport{
                 TLSClientConfig:     &tls.Config{InsecureSkipVerify: true},
                 Dial:                (&net.Dialer{Timeout: 0, KeepAlive: 0}).Dial,
                 TLSHandshakeTimeout: 5 * time.Second,
         }}
 
-        scanner := bufio.NewScanner(os.Stdin)
-        for scanner.Scan() {
-            for _, schema := range []string{"http://","https://"}{
-                for _, location := range []string{"/.well-known/security.txt","/security.txt"}{
-                    url := schema + scanner.Text() + location
-                    wg.Add(1)
-                    lock <- struct{}{}
-                    go func(url string) {
-                            defer wg.Done()
-                            defer func() { <-lock }()
-
-                            req, err := http.NewRequest("GET", url, nil)
-                            if err != nil {
-                                log.Fatal(err)
-                            }
-                            resp, err := client.Do(req)
-                            if err != nil {
-                                    //fmt.Println(999, err, url)
-                                    return
-                            }
-                            bodyString := ""
-                            req.Header.Set("Connection", "close")
-                            if resp.StatusCode == http.StatusOK {
-                                bodyBytes, err := ioutil.ReadAll(resp.Body)
-                                if err != nil {
-                                    log.Fatal(err)
-                                }
-                                bodyString = string(bodyBytes)
-                            }
-                            for _, signature := range []string{"Contact:", "Encryption:", "Acknowledgments:", "Preferred-Languages:", "Canonical:", "Policy:", "Hiring:"}{
-                                if strings.Contains(bodyString, signature){
-                                    fmt.Print("\n\n###########################\nFOUND security.txt: " + url + "\n###########################\n")
-                                    fmt.Print(bodyString)
-                                }
-                                return
-                            }
-                            resp.Body.Close()
-                            fmt.Println(resp.StatusCode, url)
-                    }(url)
+        numWorkers := *concurrencyPtr
+        work := make(chan string)
+        go func() {
+            s := bufio.NewScanner(os.Stdin)
+            for s.Scan() {
+                for _, schema := range []string{"http://","https://"}{
+                    for _, location := range []string{"/.well-known/security.txt","/security.txt"}{
+                        work <- schema + s.Text() + location
+                    }
                 }
             }
-        }
-        if err := scanner.Err(); err != nil {
-                fmt.Println(err)
+            close(work)
+        }()
+
+        wg := &sync.WaitGroup{}
+
+        for i := 0; i < numWorkers; i++ {
+            wg.Add(1)
+            go doWork(work, wg, client)
         }
         wg.Wait()
+}
+
+func doWork(work chan string, wg *sync.WaitGroup, client *http.Client) {
+    defer wg.Done()
+    for url := range work {
+        defer wg.Done()
+        req, err := http.NewRequest("GET", url, nil)
+        if err != nil {
+            fmt.Println(err)
+            continue
+        }
+        resp, err := client.Do(req)
+        if err != nil {
+                //fmt.Println(999, err, url)
+                return
+        }
+        bodyString := ""
+        req.Header.Set("Connection", "close")
+        if resp.StatusCode == http.StatusOK {
+            bodyBytes, err := ioutil.ReadAll(resp.Body)
+            if err != nil {
+                log.Fatal(err)
+            }
+            bodyString = string(bodyBytes)
+        }
+        for _, signature := range []string{"Contact:", "Encryption:", "Acknowledgments:", "Preferred-Languages:", "Canonical:", "Policy:", "Hiring:"}{
+            if strings.Contains(bodyString, signature){
+                fmt.Print("\n\n###########################\nFOUND security.txt: " + url + "\n###########################\n")
+                fmt.Print(bodyString)
+            }
+            return
+        }
+        resp.Body.Close()
+        fmt.Println(resp.StatusCode, url)
+    }
 }
