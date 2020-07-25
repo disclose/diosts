2203d488897ce5e72796293d3787e6c8402a2a5c
Giel de Nijs <giel@caffeinetrip.com>
2020-07-24 23:03:18 -0700

    Add disclose.io field struct

---
+++ internal/pkg/discloseio/discloseio.go
@@ -0,0 +1,38 @@
+package discloseio
+
+import (
+	"time"
+
+	"github.com/hakluke/haksecuritytxt/pkg/securitytxt"
+)
+
+type Fields struct {
+	ProgramName string `json:"program_name,omitempty"`
+	PolicyURL []string `json:"policy_url,omitempty"`
+	ContactURL []string `json:"contact_url"`
+	LaunchDate time.Time `json:"launch_date,omitempty"`
+	OffersBounty string `json:"offers_bounty,omitempty"`
+	OffersSwag bool `json:"offers_swag,omitempty"`
+	HallOfFame []string `json:"hall_of_fame,omitempty"`
+	SafeHarbor string `json:"safe_harbor,omitempty"`
+	PublicDisclosure string `json:"public_disclosure,omitempty"`
+	DisclosureTimelineDays int `json:"disclosure_timeline,omitempty"`
+	PGPKey []string `json:"pgp_key,omitempty"`
+	Hiring []string `json:"hiring,omitempty"`
+	SecuritytxtURL []string `json:"securitytxt_url,omitempty"`
+	PreferredLanguages string `json:"preferred_languages,omitempty"`
+}
+
+func FromSecurityTxt(txt *securitytxt.SecurityTxt) *Fields {
+	f := &Fields{
+		PolicyURL: txt.Policy,
+		ContactURL: txt.Contact,
+		HallOfFame: txt.Acknowledgements,
+		PGPKey: txt.Encryption,
+		Hiring: txt.Hiring,
+		SecuritytxtURL: txt.Canonical,
+		PreferredLanguages: txt.PreferredLanguages,
+	}
+
+	return f
+}
